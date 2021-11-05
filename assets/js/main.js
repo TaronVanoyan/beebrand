19b892fa79cb6c3ee80a03fb94a90338f4560f66
TaronVanoyan <taronraul@gmail.com>
2021-11-06 00:31:57 +0400

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.section__partners__slide').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: $('.slick-prev'),
+        nextArrow: $('.slick-next'),
+        // fade: true,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+    });
+});
